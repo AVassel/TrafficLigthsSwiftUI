ced1f31cca025ea6afdd2aa66d6a92d7c667085e
A.Vassel <102523970+AVassel@users.noreply.github.com>
2022-12-26 23:48:31 +0300

    Initial Commit

---
+++ TrafficLigthsSwiftUI/TrafficLigthsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLigthsSwiftUIApp.swift
+//  TrafficLigthsSwiftUI
+//
+//  Created by Anton Vassel on 26.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLigthsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
